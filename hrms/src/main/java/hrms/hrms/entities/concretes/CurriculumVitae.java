2d1277601c1b8d73462b68a231c0827cbb2aec62
emirhanusta <emirhan1usta@gmail.com>
2022-07-09 18:47:45 +0300

    cv entities added

---
+++ hrms/src/main/java/hrms/hrms/entities/concretes/CurriculumVitae.java
@@ -0,0 +1,44 @@
+package hrms.hrms.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurriculumVitae {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="cv_id")
+	private int cvId;
+	
+	@Column(name="covver_letter")
+	private String coverLetter;
+	
+    @OneToOne(mappedBy = "curriculumVitae")
+    private SocialMedia socialMedia;
+    
+    @OneToMany(mappedBy = "curriculumVitae")
+    private List<Experience> experience;
+    
+    @OneToMany(mappedBy = "curriculumVitae")
+    private List<Education> education;
+    
+    @OneToMany(mappedBy = "curriculumVitae")
+    private List<Language> language;
+    
+    @OneToOne(mappedBy = "curriculumVitae")
+    private Image image;
+}
---
+++ hrms/src/main/java/hrms/hrms/entities/concretes/Experience.java
@@ -0,0 +1,47 @@
+package hrms.hrms.entities.concretes;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="experiences")
+public class Experience {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="work_place")
+	private int workPlace;
+	 
+	@Column(name="position")
+	private int position;
+	
+	@Column(name = "starting_date")
+	private LocalDate startingDate;
+	
+	@Column(name = "quit_date")
+	private LocalDate quitDate;
+	
+	@ManyToOne
+	@JoinColumn(name="curriculum_vitae_id")
+	private CurriculumVitae curriculumVitae;
+	
+}
