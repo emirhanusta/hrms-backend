700e05c3fac0b479ea4da61a0c63f984445dd108
emirhanusta <emirhan1usta@gmail.com>
2022-07-11 19:01:37 +0300

    city ​​operations updated

--- hrms/src/main/java/hrms/hrms/businees/abstracts/CityService.java
+++ hrms/src/main/java/hrms/hrms/businees/abstracts/CityService.java
@@ -1,5 +1,13 @@
 package hrms.hrms.businees.abstracts;
 
-public interface CityService {
+import java.util.List;
 
+import hrms.hrms.core.utilities.results.DataResult;
+import hrms.hrms.core.utilities.results.Result;
+import hrms.hrms.entities.concretes.City;
+
+public interface CityService {
+	Result add(City city);
+
+	DataResult<List<City>> getAll();
 }
