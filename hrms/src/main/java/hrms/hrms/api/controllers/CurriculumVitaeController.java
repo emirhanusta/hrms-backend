3fca1e27e6b4802f7a413f90f7bb1d43307c5613
emirhanusta <emirhan1usta@gmail.com>
2022-07-17 16:00:23 +0300

    Curriculum vitae operations added

---
+++ hrms/src/main/java/hrms/hrms/api/controllers/CurriculumVitaeController.java
@@ -0,0 +1,39 @@
+package hrms.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hrms.hrms.businees.abstracts.CurriculumVitaeService;
+import hrms.hrms.core.utilities.results.DataResult;
+import hrms.hrms.core.utilities.results.Result;
+import hrms.hrms.entities.concretes.CurriculumVitae;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/api/curriculum_vitae")
+public class CurriculumVitaeController {
+
+	private CurriculumVitaeService curriculumVitaeService;
+
+	public CurriculumVitaeController(CurriculumVitaeService curriculumVitaeService) {
+		super();
+		this.curriculumVitaeService = curriculumVitaeService;
+	}
+	@GetMapping(value = "/getall")
+	public DataResult<List<CurriculumVitae>> getAll(){
+		return this.curriculumVitaeService.getAll();
+	}
+	@PostMapping(value="/add")
+	public Result add(@RequestBody CurriculumVitae curriculumVitae) {
+		return this.curriculumVitaeService.add(curriculumVitae);
+		
+	}
+	@PostMapping(value = "/delete")
+	public Result delete(@RequestBody CurriculumVitae curriculumVitae) {
+		return this.curriculumVitaeService.delete(curriculumVitae);
+	}
+}
---
+++ hrms/src/main/java/hrms/hrms/businees/abstracts/CurriculumVitaeService.java
@@ -0,0 +1,13 @@
+package hrms.hrms.businees.abstracts;
+
+import java.util.List;
+
+import hrms.hrms.core.utilities.results.DataResult;
+import hrms.hrms.core.utilities.results.Result;
+import hrms.hrms.entities.concretes.CurriculumVitae;
+
+public interface CurriculumVitaeService {
+	Result add(CurriculumVitae curriculumVitae);
+	Result delete(CurriculumVitae curriculumVitae);
+	DataResult<List<CurriculumVitae>> getAll();
+}
